e47fc4f13d67357ae185496fa2410f0f3649dd3f
Sava Morozov <kto4who@gmail.com>
2024-03-09 19:20:58 +0500

    nav create

---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='flex p-0 w-20 rounded-xl bg-black text-white'>
+      Menu
+    </nav>
+  )
+}
+
+export default Navbar
