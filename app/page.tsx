8cc80976e981109623d4299f5b9e09f285c0b8d5
Sava Morozov <kto4who@gmail.com>
2024-03-09 18:05:07 +0500

    setting up

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <h1>Platform for contemporary digital art</h1>
+    <h1 className="font-bold text-graynext">
+      Platform for contemporary digital art
+    </h1>
   );
 }
