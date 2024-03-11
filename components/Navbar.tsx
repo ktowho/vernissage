9ff0d943008a613155be42f4c0466b110a536197
Sava Morozov <kto4who@gmail.com>
2024-03-11 16:02:17 +0500

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='absolut p-0 w-20 rounded-xl bg-black text-white'>
+    <nav className='flex justify-center fixed bottom-8  origin-center p-0 w-20 rounded-xl bg-black text-white'>
       Menu
     </nav>
   )
