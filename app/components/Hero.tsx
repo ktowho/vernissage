6cb889952c65dde98242e2145c702e20be7db58a
Sava Morozov <kto4who@gmail.com>
2024-03-12 18:14:51 +0500

    hero text

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import localFont from '@next/font/local'
 
-const myFont = localFont({
+const neueMachinaRegularFont = localFont({
   src: '../../public/fonts/NeueMachina-Regular.woff2',
   display: 'swap',
   variable: '--font-neueMachina-Regular'
@@ -12,7 +12,7 @@ const Hero = () => {
     <>
       <div className="grid auto-cols-auto justify-center h-dvh overflow-hidden z-[-1] py-[40px]
       bg-[url('/bg.gif')] bg-[#6a134f] bg-center bg-cover bg-no-repeat bg-blend-multiply">
-        <h1 className={`${myFont.variable} font-hero text-white self-center`}> Platform for contemporary digital art</h1>
+        <h1 className={`${neueMachinaRegularFont.variable} text-center w-[700px] text-[70px] font-hero text-white self-center`}> Platform for contemporary digital art</h1>
       </div>
       
     </>
