83466e18d8a745fd9467315ea66c107b84937dac
Sava Morozov <kto4who@gmail.com>
2024-03-14 18:44:50 +0500

    Menu Icon animated

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import { Manrope } from 'next/font/google'
 import Link from 'next/link'
 
@@ -21,25 +22,29 @@ const navList = [
 
 const Navbar = () => {
 
+  const [open, setOpen] = useState(false)
+
   return (
+    <nav className={`${manropeFont.variable} fixed bottom-[40px] left-[50%] -translate-x-1/2 z-50 font-nav`}>
+      {/* Nav List  */}
 
-      <nav className={`${manropeFont.variable} fixed bottom-[40px] left-[50%] -translate-x-1/2 z-50 font-nav`}>
-        <ul className='text-white flex flex-col items-center'>
-          {navList.map((item) => (
-            <li><Link href={item.link}>{item.name}</Link></li>
-          ))}
-        </ul>
-        <div className='flex items-center w-[150px] h-[60px] justify-between px-[30px] py-[15px]
-      rounded-[20px] bg-black text-white text-[18px]'>
-          <div>
-            <span className='block w-[18px] h-[2px] bg-white mb-1' />
-            <span className='block w-[18px] h-[2px] bg-white mb-1' />
-          </div>
-          <div className='font-hero'>Menu</div>
+      <ul className={`${open ? 'flex' : 'hidden'} text-white flex-col items-center `}>
+        {navList.map((item) => (
+          <li><Link href={item.link}>{item.name}</Link></li>
+        ))}
+      </ul>
+      {/* Menu Button  */}
+      <div onClick={() => setOpen(!open)}
+        className='flex items-center w-[150px] h-[60px] justify-between px-[30px] py-[15px]
+        rounded-[20px] bg-black text-white text-[18px] hover:cursor-pointer'>
+        <div>
+          {/* Menu Icon (open/close)  */}
+          <span className={`block w-[18px] h-[2px] bg-white duration-100 ${open ? ' rotate-45 translate-x-[1px] translate-y-[0.5px]' : 'mb-1'}`} />
+          <span className={`block w-[18px] h-[2px] bg-white ${open ? '-rotate-45 translate-x-[1px] -translate-y-[1.5px]' : ''}`} />
         </div>
-      </nav>
-
-
+        <div>Menu</div>
+      </div>
+    </nav>
   )
 }
 
